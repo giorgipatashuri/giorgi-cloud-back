78c80dd1726714c6a5bc4cf92c0072f9ad6fab7e
unknown <patashuri2003@gmail.com>
2023-06-15 23:30:13 +0400

    fixed file service

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 export enum FileType {
   PHOTOS = 'photos',
@@ -10,7 +9,7 @@ export class FilesService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll(userId: number, fileType: FileType) {
-    let where: Prisma.FileWhereInput = { userId };
+    let where: any = { userId };
 
     if (fileType === FileType.PHOTOS) {
       where = { ...where, mimetype: { contains: 'image' } };
